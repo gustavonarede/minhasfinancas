dbd90d786077fd89c84d1d51b7097a45b10c7122
unknown <gustavonarede1@gmail.com>
2021-06-28 08:59:17 -0300

    commit validandoLancamento

--- src/main/java/com/adriano/minhasfinancas/service/MensagemService.java
+++ src/main/java/com/adriano/minhasfinancas/service/MensagemService.java
@@ -1,3 +1,4 @@
+package com.adriano.minhasfinancas.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
