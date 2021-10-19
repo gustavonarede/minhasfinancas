6ce6ba62cd21f8aec80082764c433a655e3d533a
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2021-10-18 21:22:00 -0300

    correcoes e log

--- src/main/java/com/adriano/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/adriano/minhasfinancas/api/resource/UsuarioResource.java
@@ -3,6 +3,8 @@ package com.adriano.minhasfinancas.api.resource;
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,45 +28,68 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UsuarioResource {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(UsuarioResource.class);
+	
 	private final UsuarioService service;
 	private final LancamentoService lancamentoService;
-	
-	
+
 	@PostMapping("/autenticar")
-	public ResponseEntity autenticar( @RequestBody UsuarioDTO dto) {
+	public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+		
+		LOGGER.info("#### Método: UsuarioResource.autenticar(), status: INICIO, nomeUsuario: "+ dto.getNome());
+		
 		try {
-		Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
-		return ResponseEntity.ok(usuarioAutenticado);
-		}catch(ErroAutenticacao e) {
+			Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+			
+			LOGGER.info("#### Método: UsuarioResource.autenticar(), status: SUCESSO, nomeUsuarioAutenticado: "+ usuarioAutenticado.getNome());
+			
+			return ResponseEntity.ok(usuarioAutenticado);
+		} catch (ErroAutenticacao e) {
+			
+			LOGGER.info("#### Método: UsuarioResource.autenticar(), status: ERROR, mensagemError: "+ e.getMessage());
+			
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
+
 	@PostMapping
 	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
-		Usuario usuario = Usuario.builder()
-				.nome(dto.getNome())
-				.email(dto.getEmail())
-				.senha(dto.getSenha())
-				.build();
+		
+		LOGGER.info("#### Método: UsuarioResource.salvar(), status: INICIO, nomeUsuario: "+ dto.getNome());
+		
+		Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
 		try {
 			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			
+			LOGGER.info("#### Método: UsuarioResource.salvar(), status: SUCESSO, nomeUsuarioSalvo: "+ usuarioSalvo.getNome());
+			
 			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
 		} catch (RegraNegocioException e) {
+			
+			LOGGER.info("#### Método: UsuarioResource.salvar(), status: ERROR, mensagemError: "+ e.getMessage());
+			
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-		
 
 	@GetMapping("{id}/saldo")
 	public ResponseEntity obterSaldo(@PathVariable("id") Long id) {
-		Optional<Usuario> usuario = service.obterPorId(id);
 		
-		if(!usuario.isPresent()) {
+		LOGGER.info("#### Método: UsuarioResource.obterSaldo(), status: INICIO, idUsuario: "+ id);
+		
+		Optional<Usuario> usuario = service.obterPorId(id);
+
+		if (!usuario.isPresent()) {
+			
+			LOGGER.info("#### Método: UsuarioResource.obterSaldo(), status: WARNING, mensagemWarning: usuário não encontrado para o id "+id);
 			return new ResponseEntity(HttpStatus.NOT_FOUND);
 		}
-		
+
 		BigDecimal saldo = lancamentoService.obterSaldoPorUsuario(id);
+		
+		LOGGER.info("#### Método: UsuarioResource.obterSaldo(), status: SUCESSO, saldo: "+ saldo);
+		
 		return ResponseEntity.ok(saldo);
 	}
-	
+
 }
--- src/main/java/com/adriano/minhasfinancas/model/entity/LancamentoDTO.java
+++ src/main/java/com/adriano/minhasfinancas/model/entity/LancamentoDTO.java
@@ -6,20 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Data
 @Builder
-
-
 @AllArgsConstructor
 public class LancamentoDTO {
 
-	public LancamentoDTO() {}
-	
+	public LancamentoDTO() {
+	}
+
 	private Long id;
 	private String descricao;
 	private Integer mes;
